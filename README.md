519835f5ffcf6eeaef8f7bc3794bd4af118c4628
Alejandro Gaviria <mr.gaviria@gmail.com>
2017-12-29 15:02:07 -0500

    part 2

---
+++ README.md
@@ -0,0 +1,5 @@
+Shipper
+-------
+A follow along for the tutorial provided by [Ewan Valentine](ewanvalentine.io)
+Check his blog out for a microservice, gRPC protobuf guide using Go and other
+cutting edge services such as docker, kubernetes, goMicro, etc.
---
+++ consignment-cli/cli.go
@@ -0,0 +1,67 @@
+// consignment-cli/cli.go
+
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	pb "github.com/agaviria/shipper/consignment-service/proto/consignment"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
+
+func parseFile(file string) (*pb.Consignment, error) {
+	var consignment *pb.Consignment
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(data, &consignment)
+	return consignment, err
+}
+
+func main() {
+	// Setup a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewShippingServiceClient(conn)
+
+	// Contact server and print out its response.
+	f := defaultFilename
+	if len(os.Args) > 1 {
+		f = os.Args[1]
+	}
+
+	consignment, err := parseFile(f)
+
+	if err != nil {
+		log.Fatalf("Could not parse file: %v", err)
+	}
+
+	r, err := client.CreateConsignment(context.Background(), consignment)
+	if err != nil {
+		log.Fatalf("Could not greet: %v", err)
+	}
+
+	log.Printf("Created: %t", r.Created)
+
+	getAll, err := client.GetConsignments(context.Background(), &pb.GetRequest{})
+	if err != nil {
+		log.Fatalf("Could not list consignments: %v", err)
+	}
+
+	for _, v := range getAll.Consignments {
+		log.Println(v)
+	}
+}
